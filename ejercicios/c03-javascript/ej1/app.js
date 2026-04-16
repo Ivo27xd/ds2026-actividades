dfe286984c68cf147d85f8169c6309359283426f
Ivo27xd <ivoinda99@gmail.com>
2026-04-16 13:29:23 -0300

    Ejercicio 1: Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1/app.js
@@ -0,0 +1,17 @@
+// declaración con const
+const nombre = "Ivo";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+//          ------------   uso de template literal ----------------------
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+// declaración con let
+let cont = 0;
+
+cont++;
+cont++;
+cont++;
+
+console.log(`Valor final del contador: ${cont}`);
+
